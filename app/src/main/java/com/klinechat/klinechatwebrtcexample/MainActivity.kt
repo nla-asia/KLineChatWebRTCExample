a2b3d5f068d7e2af406e8db0a645964e04960cc6
Nay Lin Aung <naylinaung.cet@gmail.com>
2022-12-20 23:20:18 +0630

    build updated

--- app/src/main/java/com/klinechat/klinechatwebrtcexample/MainActivity.kt
+++ app/src/main/java/com/klinechat/klinechatwebrtcexample/MainActivity.kt
@@ -1,58 +1,59 @@
 package com.klinechat.klinechatwebrtcexample
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.text.SpannableStringBuilder
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.klinechat.klinechatwebrtcexample.databinding.ActivityMainBinding
+import androidx.core.content.ContextCompat
+import com.klinechat.klinechatwebrtcexample.databinding.MainActivityBinding
+
+// imports from library
+import com.klinechat.webrtcsdk.MainViewModel
+import com.klinechat.webrtcsdk.util.requestNeededPermissions
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+    val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = MainActivityBinding.inflate(layoutInflater)
+
+        val urlString = "wss://webrtc.klinechat.com"
+        val tokenString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6IllvbG8iLCJjYW5QdWJsaXNoIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWV9LCJpYXQiOjE2NzE1NDczMTUsIm5iZiI6MTY3MTU0NzMxNSwiZXhwIjoxNjcxNTY4OTE1LCJpc3MiOiJBUEk0WENrdVdndlFnVWUiLCJzdWIiOiIwOTc5MDU1Mjc3OSIsImp0aSI6IjA5NzkwNTUyNzc5In0.S9-0iFPFlqh7hb4utowYA_7b4mps87wSBxP80Pr43dE"
+
+
+        binding.run {
+
+            deviceId.text = SpannableStringBuilder("Your deviceID : $urlString")
+
+            connectButton.setOnClickListener {
+                val intent = Intent(this@MainActivity, CallActivity::class.java).apply {
+                    putExtra(
+                        CallActivity.KEY_ARGS,
+                        CallActivity.BundleArgs(
+                            urlString,
+                            tokenString
+                        )
+                    )
+                }
+
+                startActivity(intent)
+            }
+
+
+
+        }
+
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        requestNeededPermissions()
     }
 }
---
+++ app/src/main/java/com/klinechat/klinechatwebrtcexample/dialog/DebugMenuDialog.kt
@@ -0,0 +1,27 @@
+package com.klinechat.klinechatwebrtcexample
+
+import android.R
+import android.app.Activity
+import android.app.AlertDialog
+import android.widget.ArrayAdapter
+
+import com.klinechat.webrtcsdk.CallViewModel
+
+
+fun Activity.showDebugMenuDialog(callViewModel: CallViewModel) {
+    val builder = with(AlertDialog.Builder(this)) {
+        setTitle("Debug Menu")
+
+        val arrayAdapter = ArrayAdapter<String>(this@showDebugMenuDialog, R.layout.select_dialog_item)
+        arrayAdapter.add("Simulate Migration")
+        arrayAdapter.add("Reconnect to Room")
+        setAdapter(arrayAdapter) { dialog, index ->
+            when (index) {
+                0 -> callViewModel.simulateMigration()
+                1 -> callViewModel.reconnect()
+            }
+            dialog.dismiss()
+        }
+    }
+    builder.show()
+}
